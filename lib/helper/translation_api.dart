5133a4a8ffb705ff8e8d1af36e128e00f5ef227f
Nurlan Hamidzade <56442688+tdk00@users.noreply.github.com>
2022-05-30 13:25:59 +0400

    Coxlu deyishiklikler

--- lib/helper/translation_api.dart
+++ lib/helper/translation_api.dart
@@ -5,13 +5,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../helper/language_helper.dart';
 import '../../../models/models.dart';
+import 'config_helper.dart';
 
 class TranslationApi {
   static Future<String> getTranslation( word ) async {
 
     var lang = await LanguageHelper.getLang();
 
-    final uri = Uri.parse('http://localhost/kingsmart_ci/Translation/translate/?word='+ word.toString() +'&lang=' + lang);
+    var base_url = await ConfigHelper.baseUrl();
+    final uri =Uri.parse(base_url + 'Translation/translate/?word='+ word.toString() +'&lang=' + lang);
+    print(uri);
     Response response = await get(uri);
     int statusCode = response.statusCode;
     String responseBody = response.body;
