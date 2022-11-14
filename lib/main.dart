8188fbaeea09fb1ee47b418321da383cef28030d
Nurlan Hamidzade <56442688+tdk00@users.noreply.github.com>
2022-11-14 11:56:04 +0400

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,23 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_app_desktop/pages/language_selection/language_selection.dart';
 
 
 void main() {
-  runApp( MyApp(),);
+  runApp( MyApp() );
 }
 
 class MyApp extends StatelessWidget {
-  final navigatorKey = GlobalKey<NavigatorState>();
-  Timer timer = Timer(Duration(hours: 20), () => {
-    // print('aaa')
-  });
-  void timeOutCallBack() {
-    navigatorKey.currentState?.pushAndRemoveUntil(
-        MaterialPageRoute(builder: (context) => MyApp()),
-            (Route<dynamic> route) => false);
-  }
+  const MyApp
+
+  ({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Listener(
-      onPointerDown: (PointerDownEvent event) {
-         if (timer != null)
-         {
-           timer.cancel();
-         }
-        timer = Timer(Duration(seconds: 8230), () => timeOutCallBack());
-      },
-      child: MaterialApp(
-        navigatorKey: navigatorKey,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const MyHomePage(),
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(),
     );
   }
 }
@@ -49,23 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-        body: GestureDetector(
-            child: Container(
-                decoration: const BoxDecoration(
-                    color: Colors.black,
-                    image: DecorationImage(
-                        image:AssetImage("assets/images/touchToStart.jpg"),
-                        fit:BoxFit.cover
-                    ),
-                )
-            ),
-            onTap:() async {
-
-
-          Navigator.pushAndRemoveUntil(context,
-              MaterialPageRoute(builder: (_) => const LanguageSelectionWidget()), (route) => false);
-        }
-        )
+        body: Container()
     );
   }
 }
